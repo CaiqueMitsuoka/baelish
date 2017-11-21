bb58d60d3de7f72216949657d4622de6b4db5c05
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-11-20 22:58:26 -0200

    Add link inital form

--- lib/baelish_web/controllers/links_controller.ex
+++ lib/baelish_web/controllers/links_controller.ex
@@ -4,4 +4,10 @@ defmodule BaelishWeb.LinksController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"link" => %{"value" => link}}) do
+    IO.puts link
+
+    render conn, "create.html"
+  end
 end
--- lib/baelish_web/router.ex
+++ lib/baelish_web/router.ex
@@ -5,7 +5,7 @@ defmodule BaelishWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -17,6 +17,7 @@ defmodule BaelishWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", LinksController, :new
+    post "/links", LinksController, :create
   end
 
   # Other scopes may use custom stacks.
